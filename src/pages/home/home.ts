f43b81340b844706b80dee828134379ee6be1c58
Joshua Morony <joshuamorony@Joshuas-MacBook-Pro.local>
2017-12-12 18:56:46 +1030

    initial commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,32 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, IonicPage } from 'ionic-angular';
+import { HoldingsProvider } from '../../providers/holdings/holdings';
 
+@IonicPage()
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+	constructor(private navCtrl: NavController, private holdingsProvider: HoldingsProvider) {
 
-  }
+	}
+
+	ionViewDidLoad(): void {
+		this.holdingsProvider.loadHoldings();
+	}
+
+	addHolding(): void {
+		this.navCtrl.push('AddHoldingPage');
+	}
+
+	goToCryptonator(): void {
+		window.open('https://www.cryptonator.com/api', '_system');
+	}
+
+	refreshPrices(refresher): void {
+		this.holdingsProvider.fetchPrices(refresher);
+	}
 
 }
---
+++ src/providers/holdings/holdings.ts
@@ -0,0 +1,95 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Storage } from '@ionic/storage';
+import { Observable } from 'rxjs/Observable';
+import { forkJoin } from 'rxjs/observable/forkJoin';
+
+interface Holding {
+	crypto: string,
+	currency: string,
+	amount: number,
+	value?: number
+}
+
+@Injectable()
+export class HoldingsProvider {
+
+	public holdings: Holding[] = [];
+
+	constructor(private http: HttpClient, private storage: Storage) {
+
+	}
+
+	addHolding(holding: Holding): void {
+	
+		this.holdings.push(holding);
+		this.fetchPrices();
+		this.saveHoldings();
+
+	}
+
+	removeHolding(holding): void {
+
+		this.holdings.splice(this.holdings.indexOf(holding), 1);
+		this.fetchPrices();
+		this.saveHoldings();
+
+	}
+
+	saveHoldings(): void {
+		this.storage.set('cryptoHoldings', this.holdings);
+	}
+	
+	loadHoldings(): void {
+
+		this.storage.get('cryptoHoldings').then(holdings => {
+
+			if(holdings !== null){
+				this.holdings = holdings;
+				this.fetchPrices();
+			}
+		});
+
+	}
+
+	verifyHolding(holding): Observable<any> {
+		return this.http.get('https://api.cryptonator.com/api/ticker/' + holding.crypto + '-' + holding.currency);
+	}
+
+	fetchPrices(refresher?): void {
+
+		let requests = [];
+
+		for(let holding of this.holdings){
+
+			let request = this.http.get('https://api.cryptonator.com/api/ticker/' + holding.crypto + '-' + holding.currency);
+
+			requests.push(request);
+
+		}
+
+		forkJoin(requests).subscribe(results => {
+
+			results.forEach((result: any, index) => {
+
+				this.holdings[index].value = result.ticker.price;
+
+			});
+
+			if(typeof(refresher) !== 'undefined'){
+				refresher.complete();
+			}
+
+			this.saveHoldings();
+
+		}, err => {
+
+			if(typeof(refresher) !== 'undefined'){
+				refresher.complete();
+			}
+
+		});
+
+	}
+
+}
